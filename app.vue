78cca6d64b671aeea7e90532b0a6cd509a1f933a
Lars Scherr <Lars.Scherr@telekom.de>
2024-07-23 10:51:06 +0200

    Test Setup

--- app.vue
+++ app.vue
@@ -1,6 +1,15 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <vueform @mounted="formMounted"></vueform>
 </template>
+
+<script lang="ts" setup>
+const emit = defineEmits(["mount", "form"]);
+
+onMounted(() => {
+  emit("mount");
+});
+
+const formMounted = () => {
+  emit("form");
+};
+</script>
